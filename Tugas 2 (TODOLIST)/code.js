f52e1e1b204d6bcc26d13870ffb810d35500455b
Nijika21 <ganiadriantio@gmail.com>
2025-05-07 20:13:08 +0700

    add 3nd version(more complex version) andfix problems

--- Tugas 2 (TODOLIST)/code.js
+++ Tugas 2 (TODOLIST)/code.js
@@ -1,4 +1,4 @@
-// versi 1 (sesuai PPT)
+// versi 1 (sesuai dengan PPT)
 { function tambahver1() {
     let teks = "paragraf ini akan dihapus"
     let p = document.createElement("p");
@@ -31,7 +31,7 @@ function tambahver2() {
     p.innerText = teks
     p.id = "penghapusan2";
     document.getElementById("container_output2").appendChild(p);
-    document.getElementById("inputver2").value - "";
+    document.getElementById("inputver2").value = "";
 }
 
 function hapusver2() {
@@ -42,5 +42,45 @@ function hapusver2() {
     }
     else{
         alert("Paragraf tidak ditemukan di versi ke-2!")
+    }
+}
+
+// Versi 3 (penyesuaian dengan input, hapus button di list, dan reset value)
+function tambahver3() {
+    let teks = document.getElementById("inputver3").value;
+
+    if(!teks.trim()) {
+        alert("Masukkan teks terlebih dahulu!");
+        return;
+    }
+
+    let oli = document.createElement("li");
+    oli.className = "todo-item";
+
+    let ospan = document.createElement("span");
+    ospan.textContent = teks;
+    oli.appendChild(ospan);
+
+    // tombol hapus di samping li
+    let hapusButon = document.createElement("button");
+    hapusButon.textContent= "Hapus";
+    hapusButon.className = "hapus-item";
+    hapusButon.onclick = function (){
+        oli.remove();
+    };
+    oli.appendChild(hapusButon);
+
+    document.getElementById("container_output3").appendChild(oli);
+    document.getElementById("inputver3").value = "";
+}
+
+function resetver3(){
+    let container = document.getElementById("container_output3")
+    if (container.children.length > 0) {
+        if (confirm("Apakah Anda yakin ingin menghapus semua data?")) {
+            container.innerHTML = ""
+        }
+    } else {
+        alert("Paragraf yang ingin direset tidak ditemukan di versi ke-3!")
     }
 }

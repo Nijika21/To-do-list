27f254a5dcde60b90925ed7f3a5f2c63d757fa86
Gani Adrian <ganiadriantio@gmail.com>
2025-05-12 13:31:52 +0700

    Update code

--- Tugas 2 (TODOLIST)/code.js
+++ Tugas 2 (TODOLIST)/code.js
@@ -1,16 +1,17 @@
 // tugas ke 2
-function tambahver3() {
-    let teks = document.getElementById("inputver3").value;
+function tambah() {
+    let teks = document.getElementById("input").value;
+    let inputfield = document.getElementById("input");
 
     if(!teks.trim()) {
         alert("Masukkan teks terlebih dahulu!");
         return;
     }
 
-    let oli = document.createElement("oli");
+    let oli = document.createElement("li");
     oli.className = "todo-item";
 
-    let ospan = document.createElement("ospan");
+    let ospan = document.createElement("span");
     ospan.textContent = teks;
     oli.appendChild(ospan);
 
@@ -19,25 +20,21 @@ function tambahver3() {
     hapusButton.className = "hapus-item";
     hapusButton.onclick = function() {
         oli.remove();
-        // fix bug bruh
-        adjustScroll();
     };
     oli.appendChild(hapusButton);
 
-    document.getElementById("container_output3").appendChild(oli);
-    document.getElementById("inputver3").value = "";
-
-    // scroll saat item melebihi kartu
-    scrollToBottom();
+    document.getElementById("output_container").appendChild(oli);
+    document.getElementById("input").value = "";
+    inputfield.focus()
 }
 
-function resetver3(){
-    let container = document.getElementById("container_output3")
+function reset(){
+    let container = document.getElementById("output_container")
     if (container.children.length > 0) {
         if (confirm("Apakah Anda yakin ingin menghapus semua data?")) {
             container.innerHTML = ""
         }
     } else {
-        alert("Paragraf yang ingin direset tidak ditemukan di versi ke-3!")
+        alert("Paragraf tidak ditemukan!")
     }
 }
